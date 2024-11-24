a2293ef2b2a262881e44876660b4b4b0d109a5c7
vaibhav <kadamvaibhav90@yahoo.com>
2024-11-25 00:04:56 +0530

    first spring app

---
+++ src/main/java/com/firstapp/first_spring_app/FirstSpringAppApplication.java
@@ -0,0 +1,15 @@
+package com.firstapp.first_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+
+		System.out.println("Hello everyone this is the first spring boot project");
+	}
+
+}
